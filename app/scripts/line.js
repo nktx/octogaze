4e688d43ec5fb1778e993079090bff7becb26689
nktx <nkt.unknown@gmail.com>
2016-05-20 16:32:51 +0800

    feat: change the presentation form of line guidance

--- app/scripts/line.js
+++ app/scripts/line.js
@@ -1,9 +1,9 @@
 function drawGuidance(status, start, cur) {
 
-	var FillSize = 20;
-	var FillSizeThreshold = 5;
-	var FillCapacity = 0.5;
-	var FillCapacityThreshold = 0.3;
+	var StrokeWidth = 40;
+	var StrokeWidthThresold = 5;
+	var StrokeCapacity = 0.5;
+	var StrokeCapacityThresold = 0.3
 
 	canvas.selectAll('.menu-svg .guidance').remove();
 
@@ -16,29 +16,29 @@ function drawGuidance(status, start, cur) {
 			offsetY = value.Subtract[0].Y;
 		}
 
-		var guide = value.Subtract.map(function(element){
+		var guide = value.Subtract.map(function(element, index){
 			return {
-				X: element.X + cur.X - offsetX,
-				Y: element.Y + cur.Y - offsetY
+				X: element.X + cur.X - offsetX - (cur.X - offsetX - start.X)*index/9 ,
+				Y: element.Y + cur.Y - offsetY - (cur.Y - offsetY - start.Y)*index/9 
 			};
     })
 		
 		if (guide[0]) {
-			canvas.append('circle')
-      	.attr({
-      		'cx': guide.slice(-1)[0].X,
-      		'cy': guide.slice(-1)[0].Y,
-      		'r': Math.max(value.Score*(FillSize+FillSizeThreshold)-FillSizeThreshold, 0),
-      		'fill': value.Color,
-      		'fill-opacity': Math.max(value.Score*(FillCapacity+FillCapacityThreshold)-FillCapacityThreshold, 0),
-      		'class': 'guidance'
-      	});
+
+			canvas.append('path')
+				.attr({
+					'd': line(guide),
+					'stroke': value.Color,
+					'stroke-width': value.Score*(StrokeWidth+StrokeWidthThresold)-StrokeWidthThresold +'px',
+					'stroke-opacity': value.Score*(StrokeCapacity+StrokeCapacityThresold)-StrokeCapacityThresold,
+					'class': 'guidance'
+				});
 
       canvas.append('text')
       	.attr({
       		'dx': guide.slice(-1)[0].X - 5,
       		'dy': guide.slice(-1)[0].Y + 5,
-      		'opacity': Math.max(value.Score*(FillCapacity+FillCapacityThreshold)-FillCapacityThreshold, 0),
+      		'opacity': Math.max(value.Score*(StrokeCapacity+StrokeCapacityThresold)-StrokeCapacityThresold, 0),
       		'class': 'guidance'
       	})
 				.text(value.Name);
