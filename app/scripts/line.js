8eceb7869870f12160a932b6642c35423161b14b
nktx <nkt.unknown@gmail.com>
2016-05-22 05:55:25 +0800

    feat: change the order of line gestures

--- app/scripts/line.js
+++ app/scripts/line.js
@@ -8,12 +8,6 @@ gestures.push({
 });
 
 gestures.push({
-	'Color': "#F1C40F",
-	'Name': "NW",
-	'Points': new Array(new Point(0,0), new Point(SquareSize*(-1),SquareSize*(-1)))
-});
-
-gestures.push({
 	'Color': "#2ECC71",
 	'Name': "SE",
 	'Points': new Array(new Point(0,0), new Point(SquareSize,SquareSize))
@@ -23,4 +17,10 @@ gestures.push({
 	'Color': "#3498DB",
 	'Name': "SW",
 	'Points': new Array(new Point(0,0), new Point(SquareSize*(-1),SquareSize))
+});
+
+gestures.push({
+	'Color': "#F1C40F",
+	'Name': "NW",
+	'Points': new Array(new Point(0,0), new Point(SquareSize*(-1),SquareSize*(-1)))
 });
