a747cc824941a949aa507ea8792fa19d738281ff
nktx <nkt.unknown@gmail.com>
2016-06-02 17:07:11 +0800

    feat: reverse the order of triangle

--- app/scripts/studygesture.js
+++ app/scripts/studygesture.js
@@ -44,14 +44,14 @@ if (displayedInterface == 'LINE') {
 
 	gestures.push({
 		'Color': "#2ECC71",
-		'Name': "RT",
-		'Points': new Array(new Point(0,0), new Point(SquareSize/2,SquareSize*(-1)), new Point(SquareSize,0), new Point(0,0))
+		'Name': "LT",
+		'Points': new Array(new Point(0,0), new Point(SquareSize,0), new Point(SquareSize/2,SquareSize*(-1)), new Point(0,0))
 	});
 
 	gestures.push({
 		'Color': "#F1C40F",
-		'Name': "LT",
-		'Points': new Array(new Point(0,0), new Point(SquareSize*(-1)/2,SquareSize*(-1)), new Point(SquareSize*(-1),0), new Point(0,0))
+		'Name': "RT",
+		'Points': new Array(new Point(0,0), new Point(SquareSize*(-1),0), new Point(SquareSize*(-1)/2,SquareSize*(-1)), new Point(0,0))
 	});
 } else {
 	var clockwise = new Array;
